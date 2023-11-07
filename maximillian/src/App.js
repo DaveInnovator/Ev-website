72bfb3546e1a0cad806eccb48b30aaa168bf6462
emdevelopa <gyimahsunday@gmail.com>
2023-11-08 00:59:14 +0100

    Good commit

--- maximillian/src/App.js
+++ maximillian/src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 class="text-3xl text-red-600 font-bold underline">Hello world!</h1>
       </div>
     );
   }
