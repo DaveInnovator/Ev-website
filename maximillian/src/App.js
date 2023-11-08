9b6d935b0cb4571f61c18ebee913a802971ea34d
emdevelopa <gyimahsunday@gmail.com>
2023-11-08 20:22:46 +0100

    nice

--- maximillian/src/App.js
+++ maximillian/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 class="text-3xl text-red-600 font-bold underline">Hello world!</h1>
+       <h1 className='text-red-600'>hello</h1>
       </div>
     );
   }
