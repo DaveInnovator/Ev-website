9d0f4378d6467c82b23fbd493ae7b8be5feab2cf
emdevelopa <gyimahsunday@gmail.com>
2023-11-13 14:33:03 +0100

    good commit

--- maximillian/src/App.js
+++ maximillian/src/App.js
@@ -53,6 +53,38 @@ class App extends Component {
           </div>
           <p className="text-center text-[14px] text-blue-600">SEE MORE &gt;</p>
         </div>
+        <div className="bg-black text-white pt-[6em]">
+          <div className="flex items-center flex-col gap-4 mb-12">
+            <h1 className="text-[36px]">The Journal</h1>
+            <p className="text-center w-1/2">
+              Our favorite stories about public lands and opportunities for you
+              to get involved in protecting your outdoor experience
+            </p>
+          </div>
+          <div className="px-[8em] flex justify-between mb-12">
+            <div className="flex flex-col items-center gap-4">
+              <div className="w-[30em] h-[20em] bg-blue-500"></div>
+              <p>MAY 30, 2017</p>
+              <h1 className="font-bold text-[24px]">An Unforgettable</h1>
+              <p className="text-center mt-4 ">
+                If you have one day to visit Yosemite National Park <br /> and
+                you want to make the most out of it.
+              </p>
+            </div>
+            <div className="flex flex-col items-center gap-4">
+              <div className="w-[30em] h-[20em] bg-blue-500"></div>
+              <p>MAY 30, 2017</p>
+              <h1 className="font-bold text-[24px]">An Unforgettable</h1>
+              <p className="text-center mt-4 ">
+                If you have one day to visit Yosemite National Park <br /> and
+                you want to make the most out of it.
+              </p>
+            </div>
+          </div>
+          <p className="text-center text-[14px] text-blue-600">
+            ALL POSTS &gt;
+          </p>
+        </div>
       </div>
     );
   }
